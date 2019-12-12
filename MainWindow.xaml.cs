3ce3c7ccb55285f3c47bf1a5f0c4646be38fedc6
YaoLing <Quantum_Links@outloo.com>
2019-12-12 15:58:15 +0800

    initial commit

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.Win32;
+using SpeechLib;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace TTS_WPF
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        SpVoice sp = new SpVoice();
+        string pitch= "<pitch absmiddle=\"0\"/>";
+        public MainWindow()
+        {
+            InitializeComponent();
+            ISpeechObjectTokens voices = sp.GetVoices();
+            foreach (ISpeechObjectToken item in voices)
+            {
+                ComboBoxItem comboBoxItem = new ComboBoxItem();
+                comboBoxItem.Content = item.GetDescription();
+                Speaker.Items.Add(comboBoxItem);
+            }
+            ComboBoxItem cbi = (Speaker.Items[0]) as ComboBoxItem;
+            cbi.IsSelected = true;
+        }
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            sp.Speak(string.Empty, SpeechVoiceSpeakFlags.SVSFPurgeBeforeSpeak);
+            sp.Speak(pitch+Text2Talk.Text, SpeechVoiceSpeakFlags.SVSFlagsAsync);
+        }
+
+        private void YinLiang_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            sp.Volume =(int) e.NewValue*10;
+        }
+
+        private void YuSu_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            sp.Rate = (int)e.NewValue;
+        }
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            SaveFileDialog file = new SaveFileDialog();
+            file.Title = "保存音频文件";
+            file.Filter = "音频文件|*.wav";
+            file.FilterIndex = 1;
+            if (file.ShowDialog() == true)
+            {
+                SpFileStream spFileStream = new SpFileStream();
+                spFileStream.Format.Type = SpeechAudioFormatType.SAFT16kHz16BitMono;
+                spFileStream.Open(file.FileName, SpeechStreamFileMode.SSFMCreateForWrite, false);
+                sp.AudioOutputStream = spFileStream; 
+                sp.Speak(pitch+Text2Talk.Text, SpeechVoiceSpeakFlags.SVSFlagsAsync);
+                sp.WaitUntilDone(-1);
+                sp.AudioOutputStream = null;
+                spFileStream.Close();
+            }
+        }
+        private void YuDiao_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            pitch = $"<pitch absmiddle=\"{(int)e.NewValue}\"/>";
+        }
+        private void Speaker_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            sp.Voice = sp.GetVoices().Item(Speaker.SelectedIndex);
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# Text2Speech
-文字转语音
+# 这是一个适用于Windos平台的文字转语音的软件
+- 使用了Windows平台的Microsoft Speech SDK组件开发
+- 使用的是`SpeechLib`COM组件而非.Net封装的System.Speech
+- WIN10平台中可通过修改注册表添加小娜、KangKang、YaoYao隐藏语音包
+
+#### 包含以下主要功能
+1. 可选择多个TTS语音包，包含中英文
+2. 支持调整音量、语调、语速
+3. 支持XML标记
+4. 支持语音导出为 `.wav` 格式
